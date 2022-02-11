83c24fadf91e77dfca7cc71a7ce67ca19b45bd71
MykeStv <mykestv22@gmail.com>
2022-02-11 16:06:39 -0500

    Se agregó el problema 1

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,44 @@
+import ejercicios.P1NumeroMayor;
+
+import javax.swing.plaf.synth.SynthOptionPaneUI;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        // Ingresar data
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("TALLER DE JAVA - MAYCOL RINCON");
+        System.out.println("Selecciona un problema: (ingresando el numero entero)\n");
+        System.out.println("1. Número mayor, igual o menor");
+        System.out.println("2. Ingresa dos números, verifica el mayor");
+        System.out.println("3. ");
+
+        System.out.print("\nProblema: ");
+        int choice = sc.nextInt();
+
+        switch (choice) {
+            case 1:
+                // Verifica cual de los siguientes numeros es mayor o si son iguales
+                P1NumeroMayor numeroMayor = new P1NumeroMayor(15, 15);
+                numeroMayor.devuelveNumeroMayor();
+                break;
+
+            case 2:
+                System.out.println("Hello world");
+                break;
+            case 3:
+                // hola
+                break;
+
+        }
+
+
+
+
+
+
+
     }
 }
---
+++ src/ejercicios/P1NumeroMayor.java
@@ -0,0 +1,44 @@
+package ejercicios;
+
+public class P1NumeroMayor {
+    private int num1;
+    private int num2;
+
+    // CONSTRUCTOR
+    public P1NumeroMayor() {
+
+    }
+    public P1NumeroMayor(int num1, int num2) {
+        this.num1 = num1;
+        this.num2 = num2;
+    }
+
+    // METHODS
+    public void devuelveNumeroMayor() {
+        if (this.num1 == this.num2) {
+            System.out.println("Los números "+ num1 + " y "+ num2 + ": son iguales.");
+        } else if(this.num1 >= this.num2) {
+            System.out.println("El número "+ this.num1 +" es mayor que "+ this.num2 +".");
+        } else {
+            System.out.println("El número "+ this.num2 +" es mayor que "+ this.num1 +".");
+        }
+    }
+
+
+    //GETTER && SETTER
+    public int getNum1() {
+        return num1;
+    }
+
+    public void setNum1(int num1) {
+        this.num1 = num1;
+    }
+
+    public int getNum2() {
+        return num2;
+    }
+
+    public void setNum2(int num2) {
+        this.num2 = num2;
+    }
+}
